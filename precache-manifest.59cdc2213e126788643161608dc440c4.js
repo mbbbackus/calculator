de980162ecf18b51b3ac05b119fee2eb884e349a
Michael Benjamin Backus <mbbbackus@gmail.com>
2020-07-13 13:33:44 -0400

    Updates

---
+++ precache-manifest.59cdc2213e126788643161608dc440c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee5ab2c7e3e551615ffb0d5e4f96244",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "10ce5f050107bdd19ed5",
+    "url": "/calculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "99a1d22f2f8d2ea26209",
+    "url": "/calculator/static/css/main.ebf172f9.chunk.css"
+  },
+  {
+    "revision": "10ce5f050107bdd19ed5",
+    "url": "/calculator/static/js/2.95c9aa83.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/calculator/static/js/2.95c9aa83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a1d22f2f8d2ea26209",
+    "url": "/calculator/static/js/main.43769ac0.chunk.js"
+  },
+  {
+    "revision": "a6e6e8718e6cab14b59b",
+    "url": "/calculator/static/js/runtime-main.009dadfc.js"
+  }
+]);
